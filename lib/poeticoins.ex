e2a33c019d5a58de3cbe2420c5a4487899b27616
Max VelDink <max@audaciousaardvark.com>
2021-03-31 05:15:39 -0500

    Up to GitHub Actions lesson

---
+++ lib/poeticoins.ex
@@ -0,0 +1,9 @@
+defmodule Poeticoins do
+  defdelegate available_products(), to: Poeticoins.Exchanges
+  defdelegate subscribe_to_trades(product), to: Poeticoins.Exchanges, as: :subscribe
+
+  defdelegate unsubscribe_from_trades(product), to: Poeticoins.Exchanges, as: :unsubscribe
+
+  defdelegate get_last_trade(product), to: Poeticoins.Historical
+  defdelegate get_last_trades(products), to: Poeticoins.Historical
+end
---
+++ lib/poeticoins/exchanges/coinbase_client.ex
@@ -0,0 +1,52 @@
+defmodule Poeticoins.Exchanges.CoinbaseClient do
+  alias Poeticoins.{Exchanges, Trade, Product}
+  alias Poeticoins.Exchanges.Client
+  require Client
+
+  Client.defclient exchange_name: "coinbase",
+                   host: 'ws-feed.pro.coinbase.com',
+                   port: 443,
+                   currency_pairs: ["BTC-USD", "ETH-USD", "LTC-USD",
+                                    "BTC-EUR", "ETH-EUR", "LTC-EUR"]
+
+  @impl true
+  def subscription_frames(currency_pairs) do
+    msg = %{
+      "type" => "subscribe",
+      "product_ids" => currency_pairs,
+      "channels" => ["ticker"]
+    } |> Jason.encode!()
+
+    [{:text, msg}]
+  end
+
+  @impl true
+  def handle_ws_message(%{"type" => "ticker"} = msg, state) do
+    {:ok, trade} = message_to_trade(msg)
+    Exchanges.broadcast(trade)
+    {:noreply, state}
+  end
+
+  def handle_ws_message(msg, state) do
+    IO.inspect(msg, label: "unhandled message")
+    {:noreply, state}
+  end
+
+  @spec message_to_trade(map()) :: {:ok, Trade.t()} | {:error, any()}
+  def message_to_trade(msg) do
+    with :ok <- validate_required(msg, ["product_id", "time", "price", "last_size"]),
+      {:ok, traded_at, _} = DateTime.from_iso8601(msg["time"])
+      do
+        {:ok,
+          Trade.new(
+            product: Product.new(exchange_name(), msg["product_id"]),
+            price: msg["price"],
+            volume: msg["last_size"],
+            traded_at: traded_at
+          )
+        }
+      else
+        {:error, _reason} = error -> error
+      end
+  end
+end
